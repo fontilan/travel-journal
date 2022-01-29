3c31718bb967b440c812b353bd6227bf7b55cf42
fontilan <68748054+fontilan@users.noreply.github.com>
2022-01-29 20:50:58 +0100

    fix pin svg reference

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
-import globe from "../images/globe.svg";
-
 function Header() {
   return (
     <header className="header">
-      <img className="header__globe" src={globe} alt="globe" />
+      <img className="header__globe" src="images/globe.svg" alt="globe" />
       <p className="header__text">my travel journal.</p>
     </header>
   );
